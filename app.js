da455dba6e667f09d363d7d3920ceec074e77cd4
SaibSaleem <saibsaleem786@gmail.com>
2024-12-30 02:58:01 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+function getinput(num) {
+    var result = document.getElementById("result");
+    result.value += num;
+}
+
+function clr() {
+    var result = document.getElementById("result");
+    result.value = "";
+}
+
+function del() {
+    var result = document.getElementById("result");
+    result.value = result.value.slice(0, result.value.length - 1);
+}
+
+function calculate() {
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
+}
